f17570fe14c40f242cb52774daefd7b71fa9ea37
Sachit Nanajkar <sachitnjk@gmail.com>
2023-10-20 01:04:45 +0100

    Added collider detection in vats script

---
+++ Assets/Scripts/PlayerScripts/VATSController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VATSController : MonoBehaviour
+{
+	private PlayerInput playerInput;
+	private InputAction vatsAction;
+	private InputAction lookAction;
+
+	private Camera playerCam;
+
+	private List<Collider> entityColliders;
+
+	private float originalTimeScale;
+
+	private void Start()
+	{
+		playerInput = InputProvider.GetPlayerInput();
+		vatsAction = playerInput.actions["VATSMode"];
+		lookAction = playerInput.actions["Look"];
+
+		playerCam = GetComponentInChildren<Camera>();
+
+		originalTimeScale = Time.timeScale;
+		
+		Cursor.lockState= CursorLockMode.Locked;
+	}
+
+	private void Update()
+	{
+		if(vatsAction.triggered)
+		{
+			ToggleVATS();
+		}
+		if(GameManager.Instance.vatsStatus)
+		{
+			EntityDetection();
+		}
+	}
+
+	private void ToggleVATS()
+	{
+		if(Time.timeScale > 0.1f) 
+		{
+			GameManager.Instance.SetVatsStatus(true);
+			Cursor.lockState = CursorLockMode.Confined;
+			Time.timeScale = 0.1f;
+		}
+		else
+		{
+			Cursor.lockState= CursorLockMode.Locked;
+			GameManager.Instance.SetVatsStatus(false);
+			Time.timeScale = originalTimeScale;
+		}
+	}
+
+	private void EntityDetection()
+	{
+		RaycastHit hit;
+
+		Vector3 mosPosition = Mouse.current.position.ReadValue();
+
+		Ray ray = playerCam.ScreenPointToRay(mosPosition);
+
+		if(Physics.Raycast(ray, out hit, Mathf.Infinity)) 
+		{
+			ColliderController entityColliderScript = hit.transform.gameObject.GetComponent<ColliderController>();
+			Debug.Log(hit.collider.gameObject.name);
+			if(entityColliderScript != null) 
+			{
+				entityColliders = entityColliderScript.GetCollidersList();
+
+				foreach(Collider collider in entityColliders) 
+				{
+					Debug.Log(collider.name);
+				}
+
+			}
+		}
+	}
+}
